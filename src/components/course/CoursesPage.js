9fd1b2775c5b7adb9a1c710695bb842c53d4af1d
agomez40 <lagomez40@gmail.com>
2016-09-14 17:31:43 -0500

    feat(): basic curses page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by agomez on 14/09/16.
+ */
+import React from 'react';
+
+class CoursesPage extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
